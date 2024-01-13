edda1d7ad8d993670a0abb02d4454ec1b6e43568
Shirish Koirala <shirishkoirala@gmail.com>
2024-01-13 19:38:09 +1000

    First Concept Complete

--- app/src/main/java/com/twiceyuan/galleryviewpager/GalleryTransformer.kt
+++ app/src/main/java/com/twiceyuan/galleryviewpager/GalleryTransformer.kt
@@ -6,15 +6,13 @@ import kotlin.math.abs
 
 object GalleryTransformer : ViewPager.PageTransformer {
 
-    private const val scale = 0.5f
+    private const val scale = 0.28f
 
     override fun transformPage(view: View, position: Float) {
         val scaleValue = 1 - abs(position) * scale
         view.scaleX = scaleValue
         view.scaleY = scaleValue
         view.alpha = scaleValue
-//        view.pivotX = view.width * (1f - position - if (position > 0) 0.75f else -0.75f) * scale
-//        view.pivotY = view.height / 2f
         view.elevation = 1f - abs(position)
     }
 }
--- app/src/main/java/com/twiceyuan/galleryviewpager/MainActivity.kt
+++ app/src/main/java/com/twiceyuan/galleryviewpager/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.twiceyuan.galleryviewpager
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.twiceyuan.galleryviewpager.databinding.ActivityMainBinding
 import com.twiceyuan.galleryviewpager.infiniteViewPager.InfinitePagerAdapter
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,5 +21,62 @@ class MainActivity : AppCompatActivity() {
         binding.viewPager.offscreenPageLimit = 3
         binding.viewPager.adapter = InfinitePagerAdapter(ImagePagerAdapter())
         binding.viewPager.setPageTransformer(true, GalleryTransformer)
+
+        binding.backgroundViewPager.adapter = InfinitePagerAdapter(ImagePagerAdapter())
+
+        binding.viewPager.setOnTouchListener(object : OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                binding.backgroundViewPager.onTouchEvent(event)
+                return false
+            }
+        })
+
+        binding.backgroundViewPager.setOnTouchListener(object : OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                binding.viewPager.onTouchEvent(event)
+                return false
+            }
+        })
+
+        binding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            private var mScrollState = ViewPager.SCROLL_STATE_IDLE
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
+                    binding.backgroundViewPager.setCurrentItem(position, false)
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                mScrollState = state;
+            }
+        })
+
+        binding.backgroundViewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            private var mScrollState = ViewPager.SCROLL_STATE_IDLE
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
+                    binding.viewPager.setCurrentItem(position, false)
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                mScrollState = state;
+            }
+
+        })
     }
 }
