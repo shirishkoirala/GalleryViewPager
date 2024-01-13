f27c119f9cd1ab37ae0e0cdaf4869a607680fa9a
Shirish Koirala <shirishkoirala@gmail.com>
2024-01-13 21:10:15 +1000

    New drawables added. Modified UI

--- app/src/main/java/com/twiceyuan/galleryviewpager/BackgroundImagePagerAdapter.kt
+++ app/src/main/java/com/twiceyuan/galleryviewpager/BackgroundImagePagerAdapter.kt
@@ -3,10 +3,8 @@ package com.twiceyuan.galleryviewpager
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.cardview.widget.CardView
 import androidx.viewpager.widget.PagerAdapter
 
 /**
@@ -14,7 +12,7 @@ import androidx.viewpager.widget.PagerAdapter
  * Email: i@twiceyuan.com
  * Site: http://twiceyuan.com
  */
-class ImagePagerAdapter : PagerAdapter() {
+class BackgroundImagePagerAdapter : PagerAdapter() {
 
     private var imgRes = intArrayOf(
         R.drawable.back_to_the_future,
@@ -26,21 +24,8 @@ class ImagePagerAdapter : PagerAdapter() {
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         // Initialize Views parent - child order
-        val cardView = CardView(container.context)
-        val relativeLayout = RelativeLayout(container.context)
         val imageView = AppCompatImageView(container.context)
         // Modify Views in parent - child order
-        cardView.radius = container.context.resources.getDimension(R.dimen.card_radius)
-        cardView.elevation = container.context.resources.getDimension(R.dimen.card_radius)
-        cardView.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        )
-
-        relativeLayout.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        )
 
         imageView.scaleType = ImageView.ScaleType.CENTER_CROP
         imageView.setImageResource(imgRes[position])
@@ -48,11 +33,9 @@ class ImagePagerAdapter : PagerAdapter() {
             RelativeLayout.LayoutParams.MATCH_PARENT,
             RelativeLayout.LayoutParams.MATCH_PARENT
         )
-        // Create View Hierarchy child - parent
-        relativeLayout.addView(imageView)
-        cardView.addView(relativeLayout)
-        container.addView(cardView)
-        return cardView
+
+        container.addView(imageView)
+        return imageView
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, any: Any) {
--- app/src/main/java/com/twiceyuan/galleryviewpager/MainActivity.kt
+++ app/src/main/java/com/twiceyuan/galleryviewpager/MainActivity.kt
@@ -5,8 +5,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import androidx.appcompat.content.res.AppCompatResources
 import com.twiceyuan.galleryviewpager.databinding.ActivityMainBinding
 import com.twiceyuan.galleryviewpager.infiniteViewPager.InfinitePagerAdapter
 
@@ -22,61 +21,41 @@ class MainActivity : AppCompatActivity() {
         binding.viewPager.adapter = InfinitePagerAdapter(ImagePagerAdapter())
         binding.viewPager.setPageTransformer(true, GalleryTransformer)
 
-        binding.backgroundViewPager.adapter = InfinitePagerAdapter(ImagePagerAdapter())
+        binding.backgroundViewPager.adapter = InfinitePagerAdapter(BackgroundImagePagerAdapter())
 
         binding.viewPager.setOnTouchListener(object : OnTouchListener {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                binding.backgroundViewPager.onTouchEvent(event)
+                var hackedEvent: MotionEvent? = null
+                event?.let { scopeEvent ->
+                    hackedEvent = MotionEvent.obtain(
+                        scopeEvent.downTime,
+                        scopeEvent.eventTime, scopeEvent.action, scopeEvent.x * 3f,
+                        scopeEvent.y, scopeEvent.metaState
+                    )
+                }
+                hackedEvent?.let {
+                    binding.backgroundViewPager.onTouchEvent(it)
+                }
                 return false
             }
         })
 
         binding.backgroundViewPager.setOnTouchListener(object : OnTouchListener {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                binding.viewPager.onTouchEvent(event)
+                var hackedEvent: MotionEvent? = null
+                event?.let { scopeEvent ->
+                    hackedEvent = MotionEvent.obtain(
+                        scopeEvent.downTime,
+                        scopeEvent.eventTime, scopeEvent.action, scopeEvent.x / 3f,
+                        scopeEvent.y, scopeEvent.metaState
+                    )
+                }
+                hackedEvent?.let {
+                    binding.viewPager.onTouchEvent(it)
+                }
+
                 return false
             }
-        })
-
-        binding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
-            private var mScrollState = ViewPager.SCROLL_STATE_IDLE
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-            }
-
-            override fun onPageSelected(position: Int) {
-                if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
-                    binding.backgroundViewPager.setCurrentItem(position, false)
-                }
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-                mScrollState = state;
-            }
-        })
-
-        binding.backgroundViewPager.addOnPageChangeListener(object : OnPageChangeListener {
-            private var mScrollState = ViewPager.SCROLL_STATE_IDLE
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-            }
-
-            override fun onPageSelected(position: Int) {
-                if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
-                    binding.viewPager.setCurrentItem(position, false)
-                }
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-                mScrollState = state;
-            }
-
         })
     }
 }
